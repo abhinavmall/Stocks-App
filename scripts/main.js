4cbe5dc15751d9f14752590d5eca8e6e3c49d246
Abhinav Mall <abhinav.0.mall@gmail.com>
2017-08-19 23:16:33 +0530

    React and gulp

---
+++ scripts/main.js
@@ -0,0 +1,12 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var StockChart = React.createClass({
+    render: function() {
+        return (
+            <p>AbhinavMall</p>
+        )
+    }
+});
+
+ReactDOM.render(<StockChart/>, document.querySelector('#main'));
